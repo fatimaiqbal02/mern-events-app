2dd08486692e53e868b627cbe034bd3617852b94
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-20 16:12:16 +0500

    Import and Use JWT middleware in Users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,14 @@
 import express  from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
 import { createNewUser, deleteUser, getAllUsers, 
          getSingleUser, updateUser } from "../Controllers/userController.js";
 
 const router = express.Router()              
 
-router.post('/', createNewUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.get('/:id', getSingleUser)
-router.get('/', getAllUsers)
+router.post('/',verifyAdmin, createNewUser)
+router.put('/:id',verifyAdmin, updateUser)
+router.delete('/:id',verifyAdmin, deleteUser)
+router.get('/:id',verifyAdmin, getSingleUser)
+router.get('/', verifyAdmin, getAllUsers)
 
 export default router
