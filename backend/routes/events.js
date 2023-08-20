1596c84a787e7f284e52d20ec419025de90e1ccc
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-20 16:05:33 +0500

    Implement JWT Middleware in Event Routes

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,12 +1,13 @@
 import express  from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
 import { createNewEvent, deleteEvent, getAllEvents, getFeaturedEvents, 
          getSingleEvent, getEventsBySearch, getEventsCount, updateEvent } from "../Controllers/eventController.js";
 
 const router = express.Router()              
 
-router.post('/', createNewEvent)
-router.put('/:id', updateEvent)
-router.delete('/:id', deleteEvent)
+router.post('/', verifyAdmin, createNewEvent)
+router.put('/:id', verifyAdmin, updateEvent)
+router.delete('/:id', verifyAdmin, deleteEvent)
 router.get('/:id', getSingleEvent)
 router.get('/', getAllEvents)
 router.get('/search/getEventBySearch', getEventsBySearch)
