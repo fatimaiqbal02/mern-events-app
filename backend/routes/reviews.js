924258b84210a979125f74aef2bca8ebfbe45f39
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-20 16:08:26 +0500

    Import & Use JWT Middleware in Reviews Routes

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,10 +1,12 @@
 import express  from "express";
-import { createReview, getAllReviews, getReviewByEventId, getReviewByUserId, getSingleReview } from "../Controllers/reviewController.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+import { createReview, getAllReviews, getReviewByEventId, 
+         getReviewByUserId, getSingleReview } from "../Controllers/reviewController.js";
 
 const router = express.Router()              
 
-router.post('/', createReview)
-router.get('/getReviewByUserId/:id', getReviewByUserId)
+router.post('/', verifyUser, createReview)
+router.get('/getReviewByUserId/:id', verifyAdmin, getReviewByUserId)
 router.get('/getReviewByeventId/:id', getReviewByEventId)
 router.get('/:id', getSingleReview)
 router.get('/', getAllReviews)
