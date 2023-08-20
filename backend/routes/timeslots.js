de7af2b89ff967df9e93a7f8190b87ca3d5d2e2b
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-20 16:10:18 +0500

    Import and Use JWT middleware in Timeslots Routes

--- backend/routes/timeslots.js
+++ backend/routes/timeslots.js
@@ -1,11 +1,13 @@
 import express  from "express";
-import { createTimeslot, deleteTimeslot, getAllSlots, updateTimeslot } from "../Controllers/timeSlotController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import { createTimeslot, deleteTimeslot, 
+         getAllSlots, updateTimeslot } from "../Controllers/timeSlotController.js";
 
 const router = express.Router()   
 
-router.post('/', createTimeslot)
+router.post('/', verifyAdmin, createTimeslot)
 router.get('/', getAllSlots)        
-router.put('/:id', updateTimeslot)
-router.delete('/:id', deleteTimeslot)   
+router.put('/:id',verifyAdmin, updateTimeslot)
+router.delete('/:id',verifyAdmin, deleteTimeslot)   
 
 export default router
