cc57726ca351d7ac56e11e7badf375cb7ddb70d7
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-21 10:19:38 +0500

    Setup index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import 'remixicon/fonts/remixicon.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
+
+)
