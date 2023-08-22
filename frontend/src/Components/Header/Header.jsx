e915fe69514f0d5c5f8a6376c7224512664cbd96
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-22 22:49:19 +0500

    Create Responsive Header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Button} from 'reactstrap'
+import {NavLink, Link} from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import './Header.css'
+
+const nav__links = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/about',
+    display: 'About'
+  },
+  {
+    path: '/events',
+    display: 'Events'
+  }
+
+]
 
 const Header = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
   return (
-    <div>
-      This is a header
-    </div>
+    <header className='header'>
+          
+      <div className={`menu-btn ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <i className="ri-menu-line"></i>
+      </div>
+
+      <div className="navbar__logo">
+        <img src={logo} alt=""/>
+      </div>
+
+      <nav className={`navbar__links ${isMenuOpen ? 'active' : ''}`}>
+        <ul>
+
+          {nav__links.map((item, index)=>(
+            <li className="navbar__links__item" key= {index}>
+              <NavLink to = {item.path} className= {navClass => navClass.isActive? "active__link": ""} 
+              >{item.display}</NavLink>
+            </li>
+          ))}
+
+        </ul>
+      </nav>
+
+      <div className="navbar__right__btns">   
+        <Button className='btn secondary__btn'><Link to = '/login'>Login</Link></Button>
+        <Button className='btn primary__btn'><Link to = '/register'>Register</Link></Button>
+      </div>
+  </header>
   )
 }
 
