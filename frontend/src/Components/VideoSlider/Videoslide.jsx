82f9438a703468a4a56a01bef4d4ba68c3ee36c8
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-23 20:29:05 +0500

    Rename Videoslider.jsx to Videoslide.jsx

---
+++ frontend/src/Components/VideoSlider/Videoslide.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './Videoslider.css'
+
+const VideoSlide = ({ title, description, videoSrc, active }) => {
+  return (
+    <div className="video-slider">
+      <div className={`video-slider__slide ${active ? 'active': ''}`}>
+        <div className="content">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <Button className='btn primary__btn'><Link to='/'>Learn More</Link></Button>
+        </div>
+        <video src={videoSrc} muted autoPlay loop></video>
+      </div>
+    </div>
+  );
+}
+
+export default VideoSlide;
