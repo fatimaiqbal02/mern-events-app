2ec93fc51b5ba4ab3e30f797bae05806caa6e14f
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-23 15:06:32 +0500

    Create videosData.js file

---
+++ frontend/src/assets/data/videosData.js
@@ -0,0 +1,29 @@
+const videosData = [
+    {
+      title: "Video Slider 1",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
+      videoSrc: '../assets/images/HomeVideo1.mp4'
+    },
+    {
+      title: "Video Slider 2",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
+      videoSrc: '../assets/images/HomeVideo2.mp4'
+    },
+    {
+        title: "Video Slider 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
+        videoSrc: '../assets/images/HomeVideo3.mp4'
+    },
+    {
+        title: "Video Slider 4",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
+        videoSrc: '../assets/images/HomeVideo4.mp4'
+    },
+    {
+        title: "Video Slider 5",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
+        videoSrc: '../assets/images/HomeVideo5.mp4'
+    }
+];
+  
+export default videosData;
