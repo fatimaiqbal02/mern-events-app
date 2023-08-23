7196a50798247ee973a875b083d847d1155011f9
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-23 15:13:13 +0500

    Update videosData.js

--- frontend/src/assets/data/videosData.js
+++ frontend/src/assets/data/videosData.js
@@ -1,28 +1,34 @@
+import video1 from '../images/HomeVideo1.mp4'
+import video2 from '../images/HomeVideo2.mp4'
+import video3 from '../images/HomeVideo3.mp4'
+import video4 from '../images/HomeVideo4.mp4'
+import video5 from '../images/HomeVideo5.mp4'
+
 const videosData = [
     {
       title: "Video Slider 1",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
-      videoSrc: '../assets/images/HomeVideo1.mp4'
+      videoSrc: video1
     },
     {
       title: "Video Slider 2",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
-      videoSrc: '../assets/images/HomeVideo2.mp4'
+      videoSrc: video2
     },
     {
         title: "Video Slider 3",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
-        videoSrc: '../assets/images/HomeVideo3.mp4'
+        videoSrc: video3
     },
     {
         title: "Video Slider 4",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
-        videoSrc: '../assets/images/HomeVideo4.mp4'
+        videoSrc: video4
     },
     {
         title: "Video Slider 5",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ...",
-        videoSrc: '../assets/images/HomeVideo5.mp4'
+        videoSrc: video5
     }
 ];
   
