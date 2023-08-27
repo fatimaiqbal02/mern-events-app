194670ee06835da5a5213678a67fb41fdcc2fb52
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-27 20:03:22 +0500

    Remove remixicon import statement

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM  from "react-dom/client";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import 'remixicon/fonts/remixicon.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
