713e713b175ab2d95f7565122d9c2bb1720ec96a
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-28 19:00:54 +0500

    Add Services Component in Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import VideoSlider from '../Components/VideoSlider/VideoSlider.jsx';
 import Subtitle from '../Components/Subtitle/Subtitle.jsx';
 import EventTypes from '../Components/EventTypes/EventTypes.jsx';
 import SearchBar from '../Components/SearchBar/SearchBar.jsx';
+import Services from '../Components/Services/Services.jsx';
 
 const Home = () => {
 
@@ -18,6 +19,10 @@ const Home = () => {
     {/* ----------------------------------- Search Bar section ------------------------------------------- */}
     <SearchBar/>
     {/* ------------------x---------------- Search Bar section --------------x---------------------------- */}
+    {/* ----------------------------------- Services section ------------------------------------------- */}
+    <Subtitle title={'Why Choose Us'}/>
+    <Services/>
+    {/* ------------------x---------------- Services section --------------x---------------------------- */}
     </>
   );
 }
