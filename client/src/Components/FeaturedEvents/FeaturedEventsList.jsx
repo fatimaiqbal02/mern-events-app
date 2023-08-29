cdfe8c9399dabed73a0e35df1949d2190907a291
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-30 00:52:16 +0500

    Create Responsive FeaturedEvents Component

--- client/src/Components/FeaturedEvents/FeaturedEventsList.jsx
+++ client/src/Components/FeaturedEvents/FeaturedEventsList.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import EventCard from '../EventCard/EventCard'
+import eventData from '../../assets/data/FeaturedEvents.js'
+import './FeaturedEvents.css'
 
 const FeaturedEventsList = () => {
   return (
-    <div>
-      Featured Event Lists
-      <EventCard/>
+    <div className='featured-event-section' >
+      {eventData?.map(event=>(
+        <div key={event._id}>
+            <EventCard event={event}/>
+        </div>
+     ))} 
     </div>
   )
 }
