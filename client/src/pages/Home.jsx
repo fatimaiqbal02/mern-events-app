ab95cc884dd6310fe2cd2a71d08f2ae5e3d0293d
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-29 11:56:33 +0500

    Add FeaturedTour Component in Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,25 +4,30 @@ import Subtitle from '../Components/Subtitle/Subtitle.jsx';
 import EventTypes from '../Components/EventTypes/EventTypes.jsx';
 import SearchBar from '../Components/SearchBar/SearchBar.jsx';
 import Services from '../Components/Services/Services.jsx';
+import FeaturedEventsList from '../Components/FeaturedEvents/FeaturedEventsList.jsx'
 
 const Home = () => {
 
   return (
     <>
-    {/* ----------------------------------- VideoSlider section ------------------------------------------- */}
+    {/* ----------------------------------- VideoSlider section ---------------------------------------- */}
     <VideoSlider/>
-    {/* ------------------x---------------- VideoSlider section --------------x---------------------------- */}
-    {/* ----------------------------------- Event-types section ------------------------------------------- */}
+    {/* ------------------x---------------- VideoSlider section --------------x-------------------------- */}
+    {/* ----------------------------------- Event-types section ----------------------------------------- */}
     <Subtitle title={'Events We Organize'}/>
     <EventTypes/>
-    {/* ------------------x---------------- Event-types section --------------x---------------------------- */}
-    {/* ----------------------------------- Search Bar section ------------------------------------------- */}
+    {/* ------------------x---------------- Event-types section --------------x-------------------------- */}
+    {/* ----------------------------------- Search Bar section ------------------------------------------ */}
     <SearchBar/>
-    {/* ------------------x---------------- Search Bar section --------------x---------------------------- */}
+    {/* ------------------x---------------- Search Bar section --------------x-------------------------- */}
     {/* ----------------------------------- Services section ------------------------------------------- */}
     <Subtitle title={'Why Choose Us'}/>
     <Services/>
     {/* ------------------x---------------- Services section --------------x---------------------------- */}
+    {/* ----------------------------------- Featured-Events section ------------------------------------ */}
+    <Subtitle title={'Featured Events'}/>
+    <FeaturedEventsList/>
+    {/* ------------------x---------------- Featured-Events section --------------x--------------------- */}
     </>
   );
 }
