2a655545092a96744ea58c2041f4c9212b7906d5
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-29 12:01:11 +0500

    Create FeaturedEvents.js file

---
+++ client/src/assets/data/FeaturedEvents.js
@@ -0,0 +1,100 @@
+
+const events =  [
+    {
+        "_id": "64e0d654eaf6bae394cf3d58",
+        "name": "Cultural Diversity Fest",
+        "venue": "Harmony Plaza",
+        "address": "Iqbal Town, Lahore",
+        "photo": "/event-images/event-img05.jpg",
+        "desc": "Embrace cultural diversity with music and food.",
+        "price": 500,
+        "minGuestSize": 100,
+        "maxGuestSize": 300,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d55",
+        "name": "Elegant Birthday Gala",
+        "venue": "Luxe Mansion",
+        "address": "Model Town, Lahore",
+        "photo": "/event-images/event-img02.jpg",
+        "desc": "Celebrate your special day in a luxurious setting.",
+        "price": 1500,
+        "minGuestSize": 50,
+        "maxGuestSize": 150,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d5a",
+        "name": "Gourmet Food Expo",
+        "venue": "Taste Bazaar",
+        "address": "Food Street, Lahore",
+        "photo": "/event-images/event-img07.jpg",
+        "desc": "Indulge in a culinary journey of flavors.",
+        "price": 300,
+        "minGuestSize": 50,
+        "maxGuestSize": 150,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d54",
+        "name": "Grand Wedding Celebration",
+        "venue": "Majestic Ballroom",
+        "address": "Johar Town, Lahore",
+        "photo": "/event-images/event-img01.jpg",
+        "desc": "A regal wedding celebration in a historical palace.",
+        "price": 10000,
+        "minGuestSize": 400,
+        "maxGuestSize": 1000,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d5c",
+        "name": "Innovators' Tech Conference",
+        "venue": "Digital Nexus Center",
+        "address": "IT Tower, Lahore",
+        "photo": "/event-images/event-img09.jpg",
+        "desc": "Explore the latest tech trends and innovations.",
+        "price": 1200,
+        "minGuestSize": 100,
+        "maxGuestSize": 300,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d5d",
+        "name": "Carnival-themed Birthday Bash",
+        "venue": "Funtime Park",
+        "address": "Gulshan-e-Iqbal, Lahore",
+        "photo": "/event-images/event-img10.jpg",
+        "desc": "A joyful carnival adventure for kids and families.",
+        "price": 600,
+        "minGuestSize": 30,
+        "maxGuestSize": 100,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d57",
+        "name": "Unicorn-themed Birthday",
+        "venue": "Enchanted Dreams Venue",
+        "address": "Cantt, Lahore",
+        "photo": "/event-images/event-img04.jpg",
+        "desc": "A magical birthday experience for young ones.",
+        "price": 800,
+        "minGuestSize": 20,
+        "maxGuestSize": 50,
+        "featured": true
+    },
+    {
+        "_id": "64e0d654eaf6bae394cf3d60",
+        "name": "Dinosaur Adventure Birthday",
+        "venue": "Jurassic Land Park",
+        "address": "Bahria Town, Lahore",
+        "photo": "/event-images/event-img13.jpg",
+        "desc": "Step into a prehistoric world for a roaring birthday.",
+        "price": 900,
+        "minGuestSize": 20,
+        "maxGuestSize": 50,
+        "featured": true
+    }
+]
+export default events;
