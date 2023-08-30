b8d396e0b14bc89246fdd5f7beaa6a331554fab3
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-30 22:56:04 +0500

    Create testimonials.js

---
+++ client/src/assets/data/testimonials.js
@@ -0,0 +1,47 @@
+import img1 from '../images/test1.jpg';
+import img2 from '../images/test2.jpg';
+import img3 from '../images/test3.jpg';
+import img4 from '../images/test4.jpg';
+import img5 from '../images/test5.jpg';
+import img6 from '../images/test6.jpg';
+
+const testimonialData = [
+    {
+        review: "Kudos to the this company for making our corporate retreat a memorable one. Their creativity and attention to detail added a unique touch to every aspect of the event.",
+        imgSrc: img1,
+        name: "Sabreena Khan",
+        title: "CEO, TechCorp"
+    },
+    {
+        review: "I was amazed by the exceptional event management skills of the team. They turned my daughter's birthday party into a magical experience that our family will cherish forever.",
+        imgSrc: img2,
+        name: "Farhan Ahmed",
+        title: "Happy Father"
+    },
+    {
+        review: "I had the pleasure of attending a wedding planned management team, and I must say, every detail was meticulously arranged. The decor, music, and ambiance were flawless.",
+        imgSrc: img3,
+        name: "Sara Malik",
+        title: "Wedding Guest"
+    },
+    {
+        review: "The event management company surpassed all expectations with their organization of our charity fundraiser. Their dedication and professionalism made the event a great success.",
+        imgSrc: img4,
+        name: "Saad Ibrahim",
+        title: "Founder, HH Foundation"
+    },
+    {
+        review: "I couldn't have asked for a better team to handle my product launch. They brought my vision to life and ensured that every guest was wowed by the event.",
+        imgSrc: img5,
+        name: "Aisha Mahmood",
+        title: "Entrepreneur, SGlamours"
+    },
+    {
+        review: "Attending the conference organized by this team was a delight. Their coordination and seamless execution made it a valuable experience for all attendees.",
+        imgSrc: img6,
+        name: "Zainab Qureshi",
+        title: "Tech Enthusiast"
+    },
+];
+
+export default testimonialData;
