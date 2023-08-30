592ba24af3034201f5a3569061c24c912da79827
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-30 14:15:58 +0500

    Create Responsive Experience Component

--- client/src/Components/Experience/Experience.jsx
+++ client/src/Components/Experience/Experience.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Experience.css'
 
 const Experience = () => {
   return (
-    <div>
-      Experience
+    <div className='experience-section'>
+      <div className="counter">
+         <span>12k+</span>
+         <h6>Successful Events</h6>
+      </div>
+      <div className="counter">
+         <span>2k+</span>
+         <h6>Regular Clients</h6>
+      </div>
+      <div className="counter">
+         <span>15</span>
+         <h6>Years Experience</h6>
+      </div>
+      
     </div>
   )
 }
