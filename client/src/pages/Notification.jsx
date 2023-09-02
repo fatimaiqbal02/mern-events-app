3ba19e4a2c8f8e9dd85eefa1dffe09eb48caf014
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-09-02 21:53:06 +0500

    Create Responsive Notification Page

--- client/src/pages/Notification.jsx
+++ client/src/pages/Notification.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/notification.css'
+import { RiCheckboxCircleLine } from "react-icons/ri";
 
-const Notification = () => {
+const Notification = ({ message = 'default' }) => {
   return (
-    <div>
-      This is notification page
-    </div>
+    <section className='notification'>
+      <div className="notification__container">
+          <span><i><RiCheckboxCircleLine/></i></span>
+          <h1 className="thankyou">Thank You</h1>
+          <h3>{message}</h3>
+          <button className='btn primary__btn'><Link to='/home'>Back to Home</Link></button>
+      </div>
+    </section>
   )
 }
 
